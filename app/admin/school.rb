835eb851f41190f7eb2f93d72454dc28cdf0c519
mikefab <mikefabrikant@gmail.com>
2017-08-02 13:09:11 -0400

    Remove old active_admin_importable code

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -5,40 +5,18 @@ ActiveAdmin.register School do
   active_admin_import validate: true,
   before_batch_import: proc { |import|
     # country_lookup = JSON.parse(File.read('./public/country_codes.json'))
+    # Get country code from file name
     country_code = import.file.original_filename.split('-').first.upcase
     import.headers["country_code"] = :country_code
     import.headers["datasource"] = :datasource
     import.batch_replace(:country_code, { nil =>  country_code} )
     import.batch_replace(:datasource, { nil =>  import.file.original_filename} )
-    #  importer.headers["country"] = :country
-    #  importer.batch_replace(:country, { nil =>  Thread.current['import.library_id']} )
-
-    #  binding.pry
-    #  import.file #current file used
-    #  import.resource #ActiveRecord class to import to
-    #  import.options # options
-    #  import.result # result before bulk iteration
-    #  import.headers # CSV headers
-    # import.headers.merge({country: 'country'})
-    # import.csv_lines.each do |line| line.push('Malawi') end
-    #  import.csv_lines #lines to import
-    #  import.model #template_object instance
-
   },
   after_batch_import: proc{ |import|
      #the same
   }
 
 
-  # active_admin_importable do |model, thing|
-  #   puts thing
-  #   binding.pry
-  #
-  #   #  store = Store.find_by_name(hash[:store_name])
-  #   #  hash[:store_id] = store.id
-  #   #  hash.delete(:store_name)
-  #   #  model.create!(hash)
-  # end
   actions :all
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
