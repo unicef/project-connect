0c35613eaa7ef2de245bf7d2b4b048a9d8143939
mikefab <mikefabrikant@gmail.com>
2017-10-12 11:50:01 -0400

    Add indexes on geo_validated fields

--- db/migrate/20171011214613_add_geo_validation_fields_to_schools.rb
+++ db/migrate/20171011214613_add_geo_validation_fields_to_schools.rb
@@ -2,7 +2,9 @@ class AddGeoValidationFieldsToSchools < ActiveRecord::Migration[5.1]
   def change
     change_table :schools do |t|
       t.boolean :coords_within_country
-      t.datetime :date_geo_validated
+      t.timestamp :date_geo_validated
     end
+    add_index :schools, :coords_within_country
+    add_index :schools, :date_geo_validated
   end
 end
