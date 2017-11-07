d794a9031f2efa75fc7f6921a5a7b14722413e85
mikefab <mikefabrikant@gmail.com>
2017-11-06 21:08:27 -0500

    use snake case

--- app/admin/schools.rb
+++ app/admin/schools.rb
@@ -56,7 +56,7 @@ ActiveAdmin.register School do
 # permit_params :list, :of, :attributes, :on, :model
   #permit_params :name, :country_code, :lat, :lon
   # permit_params :address, :admin0, :admin1, :admin2, :admin3, :admin4, :admin_code, :admin_id, :altitude, :availability_connectivity, :connectivity, :country, :country_code, :datasource, :description, :educ_level, :electricity, :environment, :frequency, :latency_connectivity, :lat, :lon, :name, :num_classrooms, :num_latrines, :num_teachers, :num_students, :num_sections, :phone_number, :postal_code, :speed_connectivity, :type_connectivity, :type_school, :water
-  permit_params :address, :admin0, :admin1, :admin2, :admin3, :admin4, :name, :country_code, :lat, :lon, :admin_id, :altitude, :availability_connectivity, :connectivity, :country_code, :datasource, :description, :educ_level, :electricity, :environment, :frequency, :latency_connectivity, :num_classrooms, :num_latrines, :num_teachers, :num_students, :num_sections, :phone_number, :postal_code, :speed_connectivity, :type_connectivity, :type_school, :water, :hasProbeData
+  permit_params :address, :admin0, :admin1, :admin2, :admin3, :admin4, :name, :country_code, :lat, :lon, :admin_id, :altitude, :availability_connectivity, :connectivity, :country_code, :datasource, :description, :educ_level, :electricity, :environment, :frequency, :latency_connectivity, :num_classrooms, :num_latrines, :num_teachers, :num_students, :num_sections, :phone_number, :postal_code, :speed_connectivity, :type_connectivity, :type_school, :water, :has_probe_data
 #
 # or
 #
@@ -104,7 +104,7 @@ ActiveAdmin.register School do
       f.input :educ_level
       f.input :electricity
       f.input :environment
-      f.input :hasProbeData
+      f.input :has_probe_data
       f.input :frequency
       f.input :latency_connectivity
       f.input :num_classrooms
--- db/migrate/20171107012124_add_has_probe_data.rb
+++ db/migrate/20171107012124_add_has_probe_data.rb
@@ -1,8 +1,8 @@
 class AddHasProbeData < ActiveRecord::Migration[5.1]
   def change
     change_table :schools do |t|
-      t.boolean :hasProbeData
+      t.boolean :has_probe_data
     end
-    add_index :schools, :hasProbeData
+    add_index :schools, :has_probe_data
   end
 end
